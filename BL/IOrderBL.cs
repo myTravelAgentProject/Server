39bc3aec0e078f7895683a3b4b70290b2826eb7b
324920867 <324920867@PC5-5>
2021-11-24 16:18:15 +0200

    getEventsForCalendar BL

--- BL/IOrderBL.cs
+++ BL/IOrderBL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace BL
 {
    public interface IOrderBL
     {
-
+        public Task<List<Order>> getEventsForCalendar(DateTime startDate, DateTime endDate);
     }
 }
--- BL/orderBL.cs
+++ BL/orderBL.cs
@@ -1,6 +1,8 @@
 ﻿using DL;
 using System;
 using Entity;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace BL
 {
@@ -9,7 +11,12 @@ namespace BL
         IOrderDL orderDL;
         public orderBL(IOrderDL orderDL)
         {
-         
+            this.orderDL = orderDL;
+        }
+
+        Task<List<Order>> getEventsForCalendar(DateTime startDate, DateTime endDate)
+        {
+            return orderDL.getEventsForCalendar(startDate, endDate);
         }
     }
 }
