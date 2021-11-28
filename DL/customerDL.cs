73b1edc78a35bc2c270ca0ba9b378d696989d045
michal <859michal@gmail.com>
2021-11-28 22:08:22 +0200

    try

--- DL/customerDL.cs
+++ DL/customerDL.cs
@@ -9,6 +9,12 @@ namespace DL
 {
     public class customerDL: ICustomerDL
     {
+        MyTravelAgentDBContext myTravelAgentDBContext;
+        public customerDL(MyTravelAgentDBContext myTravelAgentDBContext)
+        {
+            this.myTravelAgentDBContext = myTravelAgentDBContext;
+        }
+
         public async Task<List<Customer>> getAllCustomers()
         {
             return null;
