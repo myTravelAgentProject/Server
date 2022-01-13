04f6938ab30542030927d7b08515d45f21713ea2
חנצ'י פינקל <213548084@mby.co.il>
2022-01-13 16:24:54 +0200

    new changes after merge

--- MyTravelAgent/Controllers/BookingController.cs
+++ MyTravelAgent/Controllers/BookingController.cs
@@ -23,7 +23,7 @@ namespace MyTravelAgent.Controllers
         /*checks the orders within two months (and the importents orders) if their prices in booking had change
          then updates the orders with the change*/
         [HttpGet]
-        public  async Task<string> updateOrders()
+        public  async Task updateOrders()
         {
             return await  BookingBL.updateOrders();
         }
