7ee30a920e0938b8640896881ee8a45835668906
michal <324920867@PC5-5>
2022-03-27 19:37:58 +0300

    כלום

--- BL/IOrderBL.cs
+++ BL/IOrderBL.cs
@@ -15,7 +15,7 @@ namespace BL
         public Task<int> addNewOrder(Order newOrder);
         public Task<List<Order>> getTheLastOrders();
          public Task<List<Order>> getAllChanges();
-        public Task<List<Order>> getOrdetsBetweenDates(DateTime start, DateTime end);
+        //public Task<List<Order>> getOrdetsBetweenDates(DateTime start, DateTime end);
         public Task deleteOrder(int id);
         public Task updateOrder(Order orderToUpdate, int id);
         public Task<List<Order>> getOrsersToCheck(DateTime today);
