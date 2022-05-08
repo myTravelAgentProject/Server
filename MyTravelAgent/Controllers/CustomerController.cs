0e85f30c0654e23e583a086ca37c4c0d03d95d74
chanchy <213548084@PC5-13>
2022-05-08 17:08:57 +0300

    change in customer controller from order to orderDTO

--- MyTravelAgent/Controllers/CustomerController.cs
+++ MyTravelAgent/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
-﻿using BL;
+﻿using AutoMapper;
+using BL;
 using DTO;
 using Entity;
 using Microsoft.AspNetCore.Authorization;
@@ -17,10 +18,12 @@ namespace MyTravelAgent.Controllers
     //[Authorize]
     public class CustomerController : ControllerBase
     {
-        ICustomerBL customerBL;
-        public CustomerController(ICustomerBL customerBL)
+        ICustomerBL customerBL; 
+        IMapper mapper;
+        public CustomerController(ICustomerBL customerBL, IMapper mapper)
         {
             this.customerBL = customerBL;
+            this.mapper = mapper;
         }
 
         //returns a list of all the customers
@@ -39,9 +42,11 @@ namespace MyTravelAgent.Controllers
 
         //return a list of orders according to their customer id
         [HttpGet("{id}/orders")]
-        public async Task<List<Order>> getByCustomerId(int id)
+        public async Task<List<OrderDTO>> getByCustomerId(int id)
         {
-            return await customerBL.getAllCustomerOrders(id);
+            List<Order> orders = await customerBL.getAllCustomerOrders(id);
+            List<OrderDTO> ordersDTO = mapper.Map<List<Order>, List<OrderDTO>>(orders);
+            return ordersDTO;
         }
         //add a new customer
         [HttpPost]
